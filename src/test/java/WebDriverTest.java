321e9e0cf3c840ebfcad9edf740877996517f6ab
Nahagliivv <Rebesh.mark@gmail.com>
2021-11-11 23:00:41 +0300

    ver 0.0.5

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -31,18 +31,18 @@ public class WebDriverTest {
         JavascriptExecutor jse = (JavascriptExecutor)driver;
         jse.executeScript("window.scrollBy(0,0)");
         searchInput = driver.findElement(By.cssSelector("#table-accounts > tbody > tr > td:nth-child(8) > div > button > span"));
-        Thread.sleep(8000);
+        Thread.sleep(4000);
         searchInput.click();
         searchInput = driver.findElement(By.cssSelector("#table-accounts > tbody > tr > td:nth-child(8) > div > div > a:nth-child(1)"));
-        Thread.sleep(8000);
+        Thread.sleep(4000);
         searchInput.click();
         searchInput = driver.findElement(By.cssSelector("#main-content > div > div.opasity-block > div.card-body > div.row.align-items-stretch.ml-2.mr-2 > div:nth-child(1) > div > div.col-md-8.p-3.align-content-stretch > table.table.borderless.d-sm-table.d-table > tbody > tr:nth-child(4) > td.d-none.d-sm-none.d-md-block.d-lg-block.d-xl-block.text-right > a"));
-        Thread.sleep(8000);
+        Thread.sleep(4000);
         searchInput.click();
         searchInput = driver.findElement(By.cssSelector("#main-content > div > div.opasity-block > div.card-body > div.row.align-items-stretch.ml-2.mr-2 > div:nth-child(1) > div > div.col-md-8.p-3.align-content-stretch > table.table.borderless.d-sm-table.d-none > tbody > tr:nth-child(4) > td:nth-child(2) > div > span.input-group-append > button.btn.btn-success"));
-        Thread.sleep(8000);
+        Thread.sleep(4000);
         searchInput.click();
-        Thread.sleep(8000);
+        Thread.sleep(4000);
         driver.quit();
     }
 }
