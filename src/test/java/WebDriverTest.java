63a4099e802aa8a94bae99f282cb62d8994969f1
Nahagliivv <Rebesh.mark@gmail.com>
2021-11-11 23:26:21 +0300

    ver 0.0.8

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -8,7 +8,7 @@ public class WebDriverTest {
     public void main() throws InterruptedException {
         System.setProperty("webdriver.gecko.driver","geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
-        Dimension d = new Dimension(1296, 975);
+        Dimension d = new Dimension(1296, 1010);
         driver.manage().window().setSize(d);
         driver.get("https://openfx.by/#01");
         System.out.print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+driver.manage().window().getSize()+"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
@@ -30,7 +30,7 @@ public class WebDriverTest {
         searchInput.click();
         JavascriptExecutor jse = (JavascriptExecutor)driver;
         jse.executeScript("window.scrollBy(0,0)");
-        searchInput = driver.findElement(By.cssSelector("#table-accounts > tbody > tr > td:nth-child(8) > div > button"));
+        searchInput = driver.findElement(By.cssSelector("div.row:nth-child(7) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(8) > div:nth-child(1) > button:nth-child(1)"));
         Thread.sleep(4000);
         searchInput.click();
         searchInput = driver.findElement(By.cssSelector("#table-accounts > tbody > tr > td:nth-child(8) > div > div > a:nth-child(1)"));
@@ -42,6 +42,7 @@ public class WebDriverTest {
         searchInput = driver.findElement(By.cssSelector("table.table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(3) > a:nth-child(1)"));
         Thread.sleep(4000);
         searchInput.click();
+        //Thread.sleep(400000000);
         Thread.sleep(4000);
 
         driver.quit();
